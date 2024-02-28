37966e665833bfe29500d2accfa53b9e42cdeb07
ayushmanadhikari <unixparadoxfb@gmail.com>
2024-02-28 21:03:36 +0545

    X api keys added.. authentication problem arises 403

--- getDataX.py
+++ getDataX.py
@@ -2,22 +2,43 @@ import requests
 import json
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
+import tweepy
 
 
 
 #getting API key and secret key from apikey file
 file = open('./apikey', 'r')
 content = file.readlines()
-apiKey = content[-4]
-apiKeySecret = content[-1]
+consumerKey = content[13].strip()
+consumerSecret = content[16].strip()
+accessKey = content[6].strip()
+accessSecret = content[9].strip()
+
+keysList = [consumerKey, consumerSecret, accessKey, accessSecret]
+for key in keysList:
+    print(key)
+
 file.close()
 
 
-twitterKeys = {
-    "apikey": apiKey,
-    "apiKeySecret": apiKeySecret
+"""twitterKeys = {
+    "apikey": consumerKey,
+    "apiKeySecret": consumerSecret,
+    "accessKey": accessKey,
+    "accessSecret": accessSecret
 }
+"""
 
+
+auth = tweepy.OAuth1UserHandler(
+    consumerKey, consumerSecret, accessKey, accessSecret
+)
+
+api = tweepy.API(auth)
+
+publicTweets = api.home_timeline()
+for tweet in publicTweets:
+    print(tweet.text)
 
 
 
