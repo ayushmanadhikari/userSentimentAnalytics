210de4a39fd756f46312df13b5754c25f82e5762
ayushmanadhikari <unixparadoxfb@gmail.com>
2024-03-06 19:20:27 +0545

    authentication changed to password flow reddit

--- getDataReddit.py
+++ getDataReddit.py
@@ -5,7 +5,9 @@ import requests
 #creds constant
 CLIENT_ID = "S-V6chA1resmTNB2N7mfkw"
 CLIENT_SECRET = "JHU-fb5mY_vnzuOQ7-hgHBD7yiGQ6w"
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+USER_AGENT = "crypto data bot 101"
+USERNAME = "unixparadox"
+PASSWORD = "numbers4799"
 
 
 session = requests.Session()
@@ -13,7 +15,9 @@ session = requests.Session()
 reddit = praw.Reddit(
     client_id = CLIENT_ID,
     client_secret = CLIENT_SECRET,
-    user_agent = USER_AGENT
+    user_agent = USER_AGENT,
+    password = PASSWORD,
+    username = USERNAME
 )
 
 #using requests library.. not working condition
@@ -26,7 +30,7 @@ reddit = praw.Reddit(
 # response = requests.request("GET", url=url, headers=headers)
 # print(response.text)
 
-print(reddit.read_only)
+print(reddit.user.me())
 
 
 for submission in reddit.subreddit("test").hot(limit=10):
