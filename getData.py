5a65218dd6dfecdac3fbce8bcf4d112334f380df
ayushmanadhikari <unixparadoxfb@gmail.com>
2024-03-09 12:24:05 +0545

    minor changes to getData.py for coincap and kafka

--- getData.py
+++ getData.py
@@ -7,34 +7,49 @@ from kafka.errors import KafkaError
 import json
 
 
-#getting the market data from the api
-url = "https://api.coincap.io/v2/assets"
-payload = {}
-headers = {}
-response = requests.request("GET", url, headers=headers, data=payload)
-
 
 #defininf constants for kafka
 CONS_KAFKA_SERVER = "localhost:29092"
 CONS_KAFKA_TOPIC = "coincapData"
 
 
-#kafka producer for pushing coincap response into kafka topic
-producer = KafkaProducer(bootstrap_servers=CONS_KAFKA_SERVER)
-message = json.dumps(response.text)
-producerResponse = producer.send(CONS_KAFKA_TOPIC, message.encode('ascii'))
 
-#synchronous send code block
-try:
-    record_metadata = producerResponse.get(timeout=10)
-except KafkaError:
-    log.exception()
-    pass
+def getDataCoinCap():
+    #getting the market data from the api
+    url = "https://api.coincap.io/v2/assets"
+    payload = {}
+    headers = {}
+    print("Extracting data. Please wait......")
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response
 
 
-print(record_metadata.topic)
-print(record_metadata.partition)
+def sendToKafka(response):
+    #kafka producer for pushing coincap response into kafka topic
+    producer = KafkaProducer(bootstrap_servers=CONS_KAFKA_SERVER)
+    message = json.dumps(response.text)
+    producerResponse = producer.send(CONS_KAFKA_TOPIC, message.encode('ascii'))
 
+    #synchronous send code block
+    try:
+        record_metadata = producerResponse.get(timeout=10)
+    except KafkaError:
+        log.exception()
+        pass
+    
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+
+
+
+
+if __name__ == "__main__":
+    returnedResponse = getDataCoinCap()
+    print("Data extracted successfully!!!!")
+    print("Posting to kafka.....")
+    sendToKafka(returnedResponse)
+
+    
 
 
 
